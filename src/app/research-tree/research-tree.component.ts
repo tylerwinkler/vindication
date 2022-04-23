f47a7c88094164edf187290117b386f4ddb56eea
Tyler Winkler <tyler@tylerwinkler.com>
2022-04-23 08:44:59 -0400

    Created ResearchTree component

---
+++ src/app/research-tree/research-tree.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-research-tree',
+  templateUrl: './research-tree.component.html',
+  styleUrls: ['./research-tree.component.sass']
+})
+export class ResearchTreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
