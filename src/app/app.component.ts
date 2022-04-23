d9180328c1f54ac675183668a087b32f6d463192
Tyler Winkler <tyler@tylerwinkler.com>
2022-04-23 08:35:30 -0400

    First pass of dev console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   static app: AppComponent;
 
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.code === "F5") {
+      this.debug = !this.debug
+      event.preventDefault();
+    }
+  }
+
   ngOnInit(): void {
     AppComponent.app = this;
 
@@ -16,6 +24,21 @@ export class AppComponent {
       document.querySelectorAll(".sidebar-item.clickable").forEach((e2)=>e2.classList.remove("active"));
       e.classList.add('active');
     })});
+
+    let consoleInput = document.querySelector(".console-input");
+    if (consoleInput) {
+      consoleInput.addEventListener('keydown', (e) => {
+        let code = (<KeyboardEvent>e).code;
+        if (code === "Enter") {
+          this.executeCommand((<HTMLInputElement>e.target).value);
+          (<HTMLInputElement>e.target).value = "";
+          e.preventDefault();
+        }
+      });
+    }
+
+    // The game should start paused
+    this.paused = true;
 
    this.beginInterval();
   }
@@ -28,6 +51,28 @@ export class AppComponent {
     if (this.val % 20 == 0) { // 2 seconds
       this.advanceDay();
     }
+  }
+
+  executeCommand(cmd: string): boolean {
+    // Nothing to process
+    if (cmd.trim().length === 0) return true;
+
+    let cmdArr = cmd.toLowerCase().split(" ");
+
+    // Nothing to process
+    if (cmdArr.length === 0) return true;
+
+    if (cmdArr[0] == "advanceDay".toLowerCase()) {
+      let days = parseInt(cmdArr[1], 10) || 1;
+      this.advanceDay(days);
+    }
+    else if (cmdArr[0] == "setSpeed".toLowerCase()) {
+      let speed = parseInt(cmdArr[1], 10) || 1;
+      this.speed = speed;
+      this.beginInterval();
+    }
+
+    return true;
   }
 
   // setSpeed sets the speed to a clamped value between 0 (basically paused) and 3 for triple speed
@@ -51,20 +96,33 @@ export class AppComponent {
     this.paused = !this.paused;
   }
 
-  advanceDay(): void {
-    ++this.day;
-    if (this.day > 30) {
+  advanceDay(daysToAdvance?: number): void {
+    if (daysToAdvance === undefined) {
+      daysToAdvance = 1;
+    }
+
+    // We cant advance forward a negative amount of days
+    daysToAdvance = Math.max(0, daysToAdvance);
+
+    this.day += daysToAdvance;
+    this.daysPassed += daysToAdvance;
+
+    while (this.day > 30) {
       ++this.month;
-      this.day = 1;
+      this.day -= 30;
     }
 
-    if (this.month > 12) {
+    while (this.month > 12) {
       ++this.year;
-      this.month = 1;
+      this.month -= 12;
+      this.daysPassed -= 360;
     }
 
+    // Simulate the days passing for RNG purposes
+    for (var i = 0; i < daysToAdvance; ++i) {
     // You make $4000 + a random 0-1000 a day
     this.money += 4000 + Math.random() * 1000;
+    }
   }
 
   formattedMoney(): string {
