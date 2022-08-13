9d0cd475f82268458acd3ace072aa28a4103c2ea
Tyler Winkler <tyler@tylerwinkler.com>
2022-08-13 17:49:36 -0400

    Negative money is always formatted with neg sign

--- src/app/money/money.component.ts
+++ src/app/money/money.component.ts
@@ -15,13 +15,11 @@ export class MoneyComponent implements OnInit {
   formattedString(): string {
     let str = '$' + (Math.floor(Math.abs(this.money) * 100) / 100).toString();
 
-    if (this.signedNegative && !this.signed) {
-      if (this.money < 0) str = "-" + str;
-    }
-    else if (this.signed) {
+    if (this.signed) {
       if (this.money > 0) str = "+" + str;
-      else if (this.money < 0) str = "-" + str;
     }
+
+    if (this.money < 0) str = "-" + str;
 
     return str.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
   }
@@ -30,5 +28,4 @@ export class MoneyComponent implements OnInit {
 
   @Input() colored: boolean = false;
   @Input() signed: boolean = false;
-  @Input() signedNegative: boolean = false;
 }
